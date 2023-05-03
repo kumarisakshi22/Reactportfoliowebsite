86bea5c46cd19ec63e996d713d277b36c837a463
kumari sakshi <kumarisakshi2210@gmail.com>
2023-05-03 21:49:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Certi from './routes/Certi';
 import { Route, Routes } from "react-router-dom";
-
+import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <>
@@ -15,7 +15,7 @@ function App() {
         <Route path="/project" element={<Project />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/certi" element={<Certi/>} />
+        <Route path="/certi" element={<Certi />} />
 
       </Routes>
     </>
