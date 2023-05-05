c6b740fd2ddc358e2eed8d884f7205f5a12e6ddf
kumari sakshi <kumarisakshi2210@gmail.com>
2023-05-05 11:04:58 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import Project from "./routes/Project";
 import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Certi from './routes/Certi';
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/certi" element={<Certi />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/certi" element={<Certi />} />
+        </Routes>
+      </BrowserRouter>
     </>
+
   );
 }
 
